32b36af7da84331226591d0b2dcd017942e5f5dc
Lee Atchison <lee@leeatchison.com>
2011-11-22 17:16:44 -0800

    Allow tests to run without the engine.

--- lib/softlaunch/engine.rb
+++ lib/softlaunch/engine.rb
@@ -1,13 +1,15 @@
 class SoftLaunch
-  class Engine < Rails::Engine
-    engine_name "soft_launch"
-    isolate_namespace SoftLaunch
-    initializer 'soft_launch.controller' do |app|
-      ActiveSupport.on_load(:action_controller) do
-        include SoftLaunchApplicationController
-        helper_method :launched?
-        helper_method :softlaunch
-        before_filter :setup_soft_launch
+  if defined? Rails
+    class Engine < Rails::Engine
+      engine_name "soft_launch"
+      isolate_namespace SoftLaunch
+      initializer 'soft_launch.controller' do |app|
+        ActiveSupport.on_load(:action_controller) do
+          include SoftLaunchApplicationController
+          helper_method :launched?
+          helper_method :softlaunch
+          before_filter :setup_soft_launch
+        end
       end
     end
   end
